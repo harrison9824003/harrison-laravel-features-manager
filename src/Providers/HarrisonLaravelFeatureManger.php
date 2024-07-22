814d72a696fd56ce2d84abf07f3f1e8d420bec7c
root <admin@example.com>
2024-07-23 01:20:07 +0800

    feature manager package init

---
+++ src/Providers/HarrisonLaravelFeatureManger.php
@@ -0,0 +1,21 @@
+<?php
+namespace Harrison\LaravelFeatureManager\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HarrisonLaravelFeatureManger extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {        
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+    }
+}
